73f675542c27d2c4c0aae12095a9ccbae1aa903c
muturi25 <petermuturi74@gmail.com>
2022-01-31 21:37:22 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    let url = 'http://localhost:3000/users';
+    let body = document.querySelector('body');
+    let config = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch(url, config)
+        .then(response => response.json())
+        .then(data => {
+            // let h2 = document.createElement('h1');
+            let id = data.id;
+            body.innerHTML = id;
+            // body.append(h2)
+        }).catch(error => {
+            body.innerHTML = error;
+        })
+}
+
+submitData("peter", "petermuturi@gmail.com")
